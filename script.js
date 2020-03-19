c2b7ff37ee6f0936954f9c4f618cfe9d629cfeb9
Andreea Nenciu <andreeanen@yahoo.com>
2020-03-19 15:15:02 +0100

    Fixed  book List  and testing deleteBook()

--- script.js
+++ script.js
@@ -5,12 +5,9 @@ const maxNumberRequests = 10;
 let currentNumberRequests = 0;
 let currentNumberAddRequests = 0;
 let currentNumberApiRequests = 0;
-//const keyStatus = document.getElementById('key-status');
-function loadDocument() {
-    const getBooksButton = document.getElementById('get-books-button');
-    const bookList = document.getElementById('book-list');
-    getBooksButton.addEventListener('click', getApiKey);
-}
+let currentNumberDeleteRequests = 0;
+
+
 function getApiKey() {
     const endpoint = baseUrl + queryString;
     const keyStatus = document.getElementById('key-status');
@@ -56,20 +53,24 @@ function getApiKey() {
 };
 
 function showAddBookForm() {
-    //const addBookButton = document.getElementById('add-book-button');
     document.getElementById('form-add-book').style.display = 'block';
-    // alert('You pressed a button');
 
+}
+
+function resetBookForm() {
+    const form = document.getElementById('form-add-book');
+    form.reset();
+    form.style.display = 'none';
 }
 
 function submitBook() {
     const key = localStorage.getItem(apiKey);
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
-
     const addBookQueryString = `key=${key}&op=insert&title=${title}&author=${author}`;
     const addEndpoint = baseUrl + addBookQueryString;
     const showBooksStatus = document.getElementById('show-books-status');
+    // const bookList = document.getElementById('book-list');
     if (currentNumberAddRequests < maxNumberRequests) {
 
         fetch(addEndpoint)
@@ -78,17 +79,18 @@ function submitBook() {
             })
             .then(function (data) {
                 currentNumberAddRequests++;
+
                 if (data.status === 'success') {
-                    console.log(data.id);
                     if (currentNumberAddRequests === 1) {
-                        showBooksStatus.innerHTML = '<br>The API key was successfully generated! After 1 try.';
+                        showBooksStatus.innerHTML = '<br>The book was successfully added! After 1 try.';
                     }
                     else {
-                        showBooksStatus.innerHTML = `<br>The API key was successfully generated! After ${currentNumberAddRequests} tries.`;
+                        showBooksStatus.innerHTML = `<br>The book was successfully added! After ${currentNumberAddRequests} tries.`;
                     }
 
                     currentNumberAddRequests = 0;
                     showBooks();
+                    resetBookForm();
                 }
                 else {
                     showBooksStatus.innerHTML = 'The book could not be added to the list.. Try again';
@@ -101,7 +103,8 @@ function submitBook() {
             })
     }
     else {
-        showBooksStatus.innerHTML = 'You reached maximum number of requests to show the books list.';
+        showBooksStatus.innerHTML = 'The book could not be added to the list.. Try again.';
+        currentNumberAddRequests = 0;
     }
 
 }
@@ -114,11 +117,7 @@ function removeKey() {
     currentNumberApiRequests = 0;
     currentNumberAddRequests = 0;
     currentNumberRequests = 0;
-    showBooksStatus.innerHTML = "";
-
 }
-
-
 
 function initializePage() {
     const key = localStorage.getItem(apiKey);
@@ -127,9 +126,6 @@ function initializePage() {
         document.getElementById('add-book-button').style.display = 'block';
         console.log('init');
         showBooks();
-        // submitBook(key);
-
-
     }
     else {
         document.getElementById('content').style.display = 'none';
@@ -140,65 +136,93 @@ function showBooks() {
     const key = localStorage.getItem(apiKey);
     const getBooksQueryString = `key=${key}&op=select`;
     const newEndpoint = baseUrl + getBooksQueryString;
-    const showBooksList = document.getElementById('book-list');
     const showBooksStatus = document.getElementById('show-books-status');
-    if (currentNumberRequests < maxNumberRequests) {
 
+    if (currentNumberRequests < maxNumberRequests) {
         fetch(newEndpoint)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
                 currentNumberRequests++;
-                console.log(data);
-                // if (data.data.length === 0 && data.status === 'success') {
-                //     showBooksStatus.innerHTML = 'The list of books is empty.';
-                // }
-                // else if (data.data.length > 0 && data.status === 'success') {
-                //     showBooksList.innerHTML = 'My list with a lot of books';
-                //     currentNumberRequests = 0;
-                // }
-                // else {
-                //     showBooksStatus.innerHTML += 'The book could not be added to the list.. Try again';
-                //     console.log(currentNumberRequests);
-                //     showBooks();
-                // }
-
 
                 if (data.status === 'success') {
-                    console.log(data);
                     if (data.data.length === 0) {
-
                         showBooksStatus.innerHTML = 'The list of books is empty.';
                     }
                     else if (data.data.length > 0) {
-                        showBooksStatus.innerHTML += `<br>You have ${data.data.length} books in your bookstore!`;
-                        //showBooksList.innerHTML = 'My list with a lot of books';
-                        let bookList = document.createElement('ul');
+                        const bookListElement = document.getElementById('book-list');
+                        bookListElement.innerHTML = "";
+                        showBooksStatus.innerHTML = `<br>You have ${data.data.length} books in your bookstore!`;
+
                         for (let i = 0; i < data.data.length; i++) {
                             let item = document.createElement('li');
+                            item.className = "list-items";
                             item.appendChild(document.createTextNode(`Title:  ${data.data[i].title}      Author:  ${data.data[i].author}`));
-                            bookList.appendChild(item);
-                            document.getElementById('book-list').appendChild(bookList);
+                            // const buttonDelete = document.createElement('button');
+                            // buttonDelete.innerHTML = 'Delete';
+                            // buttonDelete.addEventListener('click', function deleteBook() {
+                            //     alert('delete book');
+                            //     const deleteBookStatus = document.getElementById('delete-book-status');
+                            //     const deleteBookQueryString = `key=${key}&op=delete&id=${data.data[i].id}`;
+                            //     const deleteBookEndpoint = baseUrl + deleteBookQueryString;
+                            //     if (currentNumberDeleteRequests < maxNumberRequests) {
 
+                            //         fetch(deleteBookEndpoint)
+                            //             .then(function (response) {
+                            //                 return response.json();
+                            //             })
+                            //             .then(function (data) {
+                            //                 currentNumberDeleteRequests++;
+                            //                 console.log(data);
+
+                            //                 if (data.status === 'success') {
+                            //                     if (currentNumberDeleteRequests === 1) {
+                            //                         showBooksStatus.innerHTML = '<br>The book was successfully deleted! After 1 try.';
+                            //                     }
+                            //                     else {
+                            //                         showBooksStatus.innerHTML = `<br>The book was successfully deleted! After ${currentNumberDeleteRequests} tries.`;
+                            //                     }
+
+                            //                     currentNumberDeleteRequests = 0;
+                            //                     showBooks();
+                            //                 }
+                            //                 else {
+                            //                     //deleteBookStatus.innerHTML = 'The book could not be deleted from the list.';
+                            //                     console.log(currentNumberDeleteRequests);
+                            //                     deleteBook();
+                            //                 }
+                            //             })
+                            //             .catch(function (error) {
+                            //                 console.log(error);
+                            //             })
+                            //     }
+                            //     else {
+                            //         showBooksStatus.innerHTML = 'The book could not be deleted from the list.. Try again.';
+                            //         currentNumberDeleteRequests = 0;
+                            //     }
+
+
+                            // });
+                            // item.appendChild(buttonDelete);
+
+                            bookListElement.appendChild(item);
                         }
-                        //document.getElementById('book-list').appendChild(bookList);
-
-
                     }
                     currentNumberRequests = 0;
                 }
                 else {
-                    //showBooksStatus.innerHTML = 'The book was not added.';
-                    console.log(currentNumberRequests);
+                    showBooksStatus.innerHTML = 'There are no books to show.';
                     showBooks();
                 }
-
-
             })
             .catch(function (error) {
                 console.log(error);
             })
+    }
+    else {
+        showBooksStatus.innerHTML = 'You reached the maximum number of tries to show the book';
+        currentNumberRequests = 0;
     }
 
 }
